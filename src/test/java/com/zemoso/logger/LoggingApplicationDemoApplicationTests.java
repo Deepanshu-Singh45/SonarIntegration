219042f266e29b970f860cb0bb195efcceba5279
Deepanshu-Singh45 <124673919+Deepanshu-Singh45@users.noreply.github.com>
2023-05-30 10:51:52 +0530

    Update LoggingApplicationDemoApplicationTests.java

--- src/test/java/com/zemoso/logger/LoggingApplicationDemoApplicationTests.java
+++ src/test/java/com/zemoso/logger/LoggingApplicationDemoApplicationTests.java
@@ -8,6 +8,10 @@ class LoggingApplicationDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Add an assertion
+		int expectedValue = 5;
+		int actualValue = 2 + 3;
+		Assertions.assertEquals(expectedValue, actualValue, "The sum should be 5.");
 	}
 
 }
